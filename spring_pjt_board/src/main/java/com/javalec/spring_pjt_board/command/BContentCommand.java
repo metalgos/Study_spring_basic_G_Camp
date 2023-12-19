1ed227454bdd89df1ef304ef4567f7ca369bd654
김용선 <107980485+metalgos@users.noreply.github.com>
2023-12-19 16:18:23 +0900

    게시판 조회서비스

--- spring_pjt_board/src/main/java/com/javalec/spring_pjt_board/command/BContentCommand.java
+++ spring_pjt_board/src/main/java/com/javalec/spring_pjt_board/command/BContentCommand.java
@@ -10,13 +10,13 @@ import com.javalec.spring_pjt_board.dao.BDao;
 import com.javalec.spring_pjt_board.dto.BDto;
 
 public class BContentCommand implements BCommand {
-
+//게시판 컨텐츠 를 보는 서비스
 	@Override
 	public void execute(Model model) {
 		// TODO Auto-generated method stub
 
-		Map<String, Object> map = model.asMap();
-		HttpServletRequest request = (HttpServletRequest) map.get("request");
+		Map<String, Object> map = model.asMap(); //모델에 담긴 값을 맵으로 변환한다.
+		HttpServletRequest request = (HttpServletRequest) map.get("request");//리스트에서 가져온 컨텐츠
 		String bId = request.getParameter("bId");
 		
 		BDao dao = new BDao();
