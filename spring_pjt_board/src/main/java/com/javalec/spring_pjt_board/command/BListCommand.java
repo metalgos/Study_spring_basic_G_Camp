d8d8151cf080442a79c95dab5ad39a34561c2294
김용선 <107980485+metalgos@users.noreply.github.com>
2023-12-19 17:05:25 +0900

    게시판 리스트 서비스 작성

--- spring_pjt_board/src/main/java/com/javalec/spring_pjt_board/command/BListCommand.java
+++ spring_pjt_board/src/main/java/com/javalec/spring_pjt_board/command/BListCommand.java
@@ -12,7 +12,7 @@ public class BListCommand implements BCommand {
 	@Override
 	public void execute(Model model) {
 		// TODO Auto-generated method stub
-		
+		//데이터에서 게시판 리스트를 읽어와 담아온다
 		BDao dao = new BDao();
 		ArrayList<BDto>dtos =  dao.list();
 		
