b748c826493c5e77651955568ede147567afb7f5
김용선 <107980485+metalgos@users.noreply.github.com>
2023-12-19 14:13:35 +0900

    Update HomeController.java
    14-1 get post 구분

--- spring_11_3_ex1_springex/src/main/java/com/javalec/spring_11_2_ex1_springex/HomeController.java
+++ spring_11_3_ex1_springex/src/main/java/com/javalec/spring_11_2_ex1_springex/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
+//get post방식 구분. mothd=RequestMethod.GET POST사용
 @Controller
 public class HomeController {
 	
