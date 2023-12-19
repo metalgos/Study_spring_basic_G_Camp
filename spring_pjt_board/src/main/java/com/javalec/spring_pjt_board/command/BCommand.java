98a3b03e0fa34468d62703c5295b177d97a295c7
김용선 <107980485+metalgos@users.noreply.github.com>
2023-12-19 16:08:47 +0900

    인터페이스, 이걸 구현하여 각 서비스에 맞게 만든다

--- spring_pjt_board/src/main/java/com/javalec/spring_pjt_board/command/BCommand.java
+++ spring_pjt_board/src/main/java/com/javalec/spring_pjt_board/command/BCommand.java
@@ -3,7 +3,7 @@ package com.javalec.spring_pjt_board.command;
 import org.springframework.ui.Model;
 
 public interface BCommand {
-
+//인터페이스, 이걸 구현하여 각 서비스에 맞게 만든다
 	public void execute(Model model);
 	
 }
