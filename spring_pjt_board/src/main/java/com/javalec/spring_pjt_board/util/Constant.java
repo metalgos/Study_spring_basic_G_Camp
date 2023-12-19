7f266c9d01a910a3196fee7f0819f46c88eb7ce4
김용선 <107980485+metalgos@users.noreply.github.com>
2023-12-19 18:24:30 +0900

    template을 어디에서도 쓸수 있도록 생성

--- spring_pjt_board/src/main/java/com/javalec/spring_pjt_board/util/Constant.java
+++ spring_pjt_board/src/main/java/com/javalec/spring_pjt_board/util/Constant.java
@@ -2,6 +2,8 @@ package com.javalec.spring_pjt_board.util;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 
+
+//template을 어디에서도 쓸수 있도록 생성
 public class Constant {
 	
 	public static JdbcTemplate template;
